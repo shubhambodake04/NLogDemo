c5d58e16ec2d4347eba0b94ba679ed17074723ad
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-24 19:29:19 +0530

    Add project files.

---
+++ NLogDemo/NLog.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogDemo
+{
+ 
+        public class NLog
+        {
+                public Logger logger = LogManager.GetCurrentClassLogger();
+                public void LogDebug(string input)
+                {
+                    logger.Debug(input);
+                }
+                public void LogError(string input)
+                {
+                   logger.Error(input);
+                }
+                public void LogWarn(string input)
+                {
+                    logger.Warn(input);
+                }
+                public void LogInfo(string input)
+                {
+                    logger.Info(input);
+                }
+
+        }
+    
+}
---
+++ NLogDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumber obj = new AddNumber();
+            obj.Sum(2,0);
+            Console.WriteLine("This is a Logger Demo");
+            Console.ReadLine();
+        }
+    }
+}
